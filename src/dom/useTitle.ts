be4d4efae44f5dbd97df5be33ae426c435996875
gisele <17761608@qq.com>
2022-07-15 15:30:21 +0800

    fix:useTitle优化

--- src/dom/useTitle.ts
+++ src/dom/useTitle.ts
@@ -1,21 +1,12 @@
-import { useEffect, useRef } from "react"
-import { useUnmount } from "common-hook"
-import { isBrowser } from "common-screw"
+import { useEffect } from "react"
 
 /**
  * @name 设置页面标题
  * @example
  * useTitle('Page Title')
  */
-export const useTitle = (title: string, options: any) => {
-  const titleRef = useRef(isBrowser ? document.title : "")
+export const useTitle = (title: string) => {
   useEffect(() => {
     document.title = title
   }, [title])
-
-  useUnmount(() => {
-    if (options.restoreOnUnmount) {
-      document.title = titleRef.current
-    }
-  })
 }
